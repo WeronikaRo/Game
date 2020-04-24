1f5bb3a78047a54833ed8ae12f1f99591bda74fb
WeronikaRo <49307070+WeronikaRo@users.noreply.github.com>
2020-04-24 12:02:31 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {y: 300},
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+var platforms;
+var player;
+
+function preload(){
+    this.load.image('day', 'image/day.png');
+    this.load.image('floor', 'image/floor.png');
+}
+
+
+function create(){
+    this.add.image(400, 300, 'day');
+    platforms = this.physics.add.staticGroup();
+
+    platforms.create(400, 567, 'floor').setScale(2).refreshBody();
+    platforms.create(150, 350, 'floor');
+    platforms.create(550, 225, 'floor');
+    platforms.create(700, 450, 'floor');
+
+}
+
+function update (){
+
+}
